b83753237c20514c9a17e0d34ecc2bd3d2cfb0e8
Deepanshu Mishra <d4deepanshu723@gmail.com>
2024-01-06 19:32:09 +0530

    backend 1st

---
+++ models/Post.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: false,
+    },
+    description: {
+      type: String,
+      required: true,
+      unique: false,
+    },
+    photo: {
+      type: String,
+      required: false,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
